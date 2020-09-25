bca8dada5076c31b1e6177f8a7ececda01533a2d
Stephen Connolly <stephen.alan.connolly@gmail.com>
2020-09-25 12:54:21 +0100

    Add 1.6.0 tap of k3d

---
+++ Formula/k3d@1.6.0.rb
@@ -0,0 +1,27 @@
+class K3dAT160 < Formula
+  desc "Little helper to run Rancher Lab's k3s in Docker"
+  homepage "https://github.com/rancher/k3d"
+  url "https://github.com/rancher/k3d/archive/v1.6.0.tar.gz"
+  sha256 "a6c6b9680e2026cdbcaf78ce97269994c0b117c4e2515e89129aecb67334a0e9"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build",
+           "-mod", "vendor",
+           "-ldflags", "-s -w -X github.com/rancher/k3d/version.Version=v#{version} " \
+                       "-X github.com/rancher/k3d/version.K3sVersion=v1.0.1",
+           "-trimpath", "-o", bin/"k3d"
+    prefix.install_metafiles
+  end
+
+  test do
+    assert_match "k3d version v#{version}", shell_output("#{bin}/k3d --version")
+    code = if File.exist?("/var/run/docker.sock")
+      0
+    else
+      1
+    end
+    assert_match "Checking docker...", shell_output("#{bin}/k3d check-tools 2>&1", code)
+  end
+end
