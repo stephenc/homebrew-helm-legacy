7247f66e4d556831e4577b775831ee19a6198fdd
Stephen Connolly <stephen.alan.connolly@gmail.com>
2020-09-25 12:39:25 +0100

    Add the k3d 1.7.0 formula

---
+++ Formula/k3d@1.7.0.rb
@@ -0,0 +1,28 @@
+class K3dAT170 < Formula
+  desc "Little helper to run Rancher Lab's k3s in Docker"
+  homepage "https://github.com/rancher/k3d"
+  url "https://github.com/rancher/k3d/archive/v1.7.0.tar.gz"
+  sha256 "e741809eb27f707c0f22c19a41ebbd6be7c20ec275285bb12bbf437a675aafb7"
+  license "MIT"
+  revision 1
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build",
+           "-mod", "vendor",
+           "-ldflags", "-s -w -X github.com/rancher/k3d/version.Version=v#{version}",
+           "-trimpath", "-o", bin/"k3d"
+    prefix.install_metafiles
+  end
+
+  test do
+    assert_match "k3d version v#{version}", shell_output("#{bin}/k3d --version")
+    code = if File.exist?("/var/run/docker.sock")
+      0
+    else
+      1
+    end
+    assert_match "Checking docker...", shell_output("#{bin}/k3d check-tools 2>&1", code)
+  end
+end
